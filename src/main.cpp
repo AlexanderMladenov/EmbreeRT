50d9f9e68f8a6bbbaade3f34522eadaa71a2e803
AlexanderMladenov <alexander.r.mladenov@gmail.com>
2014-10-17 18:33:22 +0300

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -76,7 +76,7 @@ int main(int argc, char* argv[])
     rtcInit(NULL);
     rtcSetErrorFunction(error_handler);
 
-    auto objData = readOBJ("../meshes/sphere.obj");
+    auto objData = readOBJ("../meshes/sponza.obj");
 
     auto& verts = std::get<0>(objData);
     auto& tris = std::get<3>(objData);
@@ -103,7 +103,7 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    Camera cam(vec3(0, 0, -300), vec3(0, 0, 0), 100);
+    Camera cam(vec3(150, 120, 0), vec3(0, 90, 0), 100);
     auto t1 = std::chrono::high_resolution_clock::now();
     RenderToBuffer4(cam, FrameBuf, scene);
     auto t2 = std::chrono::high_resolution_clock::now();
